5d0bfd78a2f642fef1b69ead9615d6dfd8b75f4b
h1gf68 <abdullaevismail39@gmail.com>
2020-10-10 17:01:13 +0300

    remove lesson1 from branch lesson2

--- lesson1/task2_secondsDevide.py
+++
@@ -1,8 +0,0 @@
-allSeconds = int(input("Введите количество секунд: "))
-if (allSeconds >= 0):
-    hours = allSeconds // 3600
-    minutes = (allSeconds % 3600) // 60
-    seconds = (allSeconds % 3600) % 60
-    print("Результат разбиения: {:02}:{:02}:{:02}".format(hours, minutes, seconds))
-else:
-    print("Количество секунд не может быть меньше нуля")
--- lesson1/task3_summN+NN+NNN.py
+++
@@ -1,4 +0,0 @@
-number = input("Введите число: ")
-
-summ = int(number) + int(number * 2) + int(number * 3)
-print(summ)
