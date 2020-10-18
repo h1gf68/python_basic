f68606e03364adf42dd167ca1317c426f70386de
h1gf68 <abdullaevismail39@gmail.com>
2020-10-18 21:16:45 +0300

    delete lesson2/

--- lesson2/task1_type().py
+++
@@ -1,4 +0,0 @@
-difTypeList = [32, 5.5, True, "any str", [2, 3, "www"], {"all", 77.1}, (3+5j, 44), 2+4j]
-
-for item in difTypeList:
-    print(type(item))
--- lesson2/task4_newWordOnNewLine.py
+++
@@ -1,6 +0,0 @@
-while True:
-    newStr = input("Введите строку из нескольких строк, разделенных пробелами:\n")
-    if not newStr:  # если введенное значение пустое выходим из цикла
-        break
-    for num, word in enumerate(newStr.split(" "), 1):
-        print(num, word[:10])
